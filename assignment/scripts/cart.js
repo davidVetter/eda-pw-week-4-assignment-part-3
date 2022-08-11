f2cd479b1709ee89d8bf06f7aee1d1e9726cbf14
David Vetter <ddvetter23@gmail.com>
2022-08-11 17:10:52 -0500

    added more tests and check if array is empty before emptying

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -23,7 +23,7 @@ console.log( `Basket is now ${basket.join(', ')}` );
 function listItems( array ) { 
     let arr = array; // variable to store argument
     let counter = 0; // setting counter to 0     ** I'm digging the for of loops so I opted to use one here **
-    for ( value of arr ) { // start for of loop  ** I can see an argument for using a reg for loop since it **
+    for ( let value of arr ) { // start for of loop  ** I can see an argument for using a reg for loop since it **
         counter += 1; // add 1 to counter        ** has a built in counter though **
         console.log(`Item #${counter} is ${value}`); // log number item is in list and item
     }
@@ -41,24 +41,25 @@ function listItems( array ) {
 
  // Function that clears an array
  function empty( array ) {
-    if ( array.length < 1 ) { // checks if array is already empty, if it is gives message and exits function
-        console.log("It's tough to clear what is already empty! Please try again...");
-        return array;
-    } else { // If array is not empty then we empty it, logging a message first of what we cleared
-    console.log(`I'm about to clear the array of ${array.join(', ')}`);
-    array.length = 0; // clearing array by setting it's length to 0
-    return array; // return the emptied array
+     if (array.length < 1) { // checks if array is already empty, if it is gives message and exits function
+         console.log("It's tough to clear what is already empty! Please try again...");
+         return array;
+     } else { // If array is not empty then we empty it, logging a message first of what we cleared
+         console.log(`I'm about to clear the array of ${array.join(', ')}`);
+         array.length = 0; // clearing array by setting it's length to 0
+         return array; // return the emptied array
     } // end if/else
  } // end empty function
 
  // calls and logs to test function with variables changed first by other functions, unchanged arrays of strings and number
- console.log('Ran function on basket', empty(basket));
+ console.log(`Showing contents of basket before the empty function: ${basket}`);
+ empty(basket);
  console.log('Basket is now ', basket);
  console.log('The array testArray is ', testArray);
- console.log('Ran function on testArray', empty(testArray));
+ empty(testArray);
  console.log('testArray is now ', testArray);
  console.log('The array testString is ', testString);
- console.log('Ran function on testString', empty(testString));
+ empty(testString);
  console.log('testString is now ', testString);
  console.log('This should display empty ', empty([ 3, 4, 5, 6] )); // emptied hard coded array
  console.log('This should display empty ', empty([])); // passing empty array
