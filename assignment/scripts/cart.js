daed7ee8212e3e14c63759fdf77f7da8e7b4931f
David Vetter <ddvetter23@gmail.com>
2022-08-12 20:44:04 -0500

    added css sheet and button to run test functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -11,7 +11,7 @@ const maxItems = 5; // global constant for stretch #1
 function addItem( item, array ) {
     let newItem = item; // create variable for 1st argument
     let arr = array; // create variable for 2nd argument
-    if (arr.length >= maxItems) { // checks if array is less than the max allowed
+    if (isFull(arr)) { // checks if array is less than the max allowed
         console.log('The list is already full! Your item was not added.'); // message if item wasn't added
         return false;
     } else {
@@ -20,6 +20,7 @@ function addItem( item, array ) {
     } // end if/else
 } // end addItem function
 
+function testAdd() {
 console.log( `Basket is ${basket}` ); // console logs to show working function
 console.log( 'Adding apples (should show true) ', addItem( 'apples', basket ));
 console.log( `Basket is now ${basket}` );
@@ -33,6 +34,7 @@ console.log( 'Adding grapes (should show true) ', addItem( 'grapes', basket ));
 console.log( `Basket is now ${basket.join(', ')}` );
 console.log( 'Adding strawberry (should show false) ', addItem( 'strawberry', basket ));
 console.log( `Basket is now ${basket.join(', ')}` );
+}
 
 // Function that reads the array basket and prints each item on a new console line, numbers items and gives message if nothing to list
 function listItems( array ) { 
@@ -48,11 +50,13 @@ function listItems( array ) {
     return; // end function
  } // end listItems function
 
+ function testList() {
  // Calls to function to test - one from global array by name, hard coded array passed as argument, array of numbers and an empty array
  listItems( basket );
  listItems( ['Dog', 'Cat', 'Fish', 'Rabbit'] );
  listItems( [ 3, 5, 7, 8, 9 ]);
  listItems( [] );
+ }
 
  // Function that clears an array
  function empty( array ) {
@@ -66,6 +70,7 @@ function listItems( array ) {
     } // end if/else
  } // end empty function
 
+ function testEmpty() {
  // calls and logs to test function with variables changed first by other functions, unchanged arrays of strings and number
  console.log(`Showing contents of basket before the empty function: ${basket.join(', ')}`);
  empty(basket);
@@ -79,11 +84,13 @@ function listItems( array ) {
  console.log('This should display empty ', empty([ 3, 4, 5, 6] )); // emptied hard coded array
  console.log('This should display empty ', empty([])); // passing empty array
  console.log(`Double checking that basket is actually empty, ${basket}, nothing should be between the commas`);
+ } // end testEmpty function
 
- //function to check if basket is full
+ // Function to check if basket is full, accepts array as argument
+ // **Tests in addItem function**
 function isFull ( array ) {
     let arr = array;
-    if ( arr < maxItems ) {
+    if ( arr.length < maxItems ) {
         return false;
     } else {
         return true;
@@ -103,16 +110,28 @@ function removeItem( item, array ) {
     } // end if else 
 } // end removeItem function
 
-// adding items to basket for testing and trying to remove an item that doesn't exist
+function testRemove() {
+// Adding items to basket for testing and trying to remove an item that doesn't exist
 console.log( 'Adding apples (should show true) ', addItem( 'apples', basket ));
 console.log( 'Adding bananas (should show true) ', addItem( 'bananas', basket ));
 console.log( 'Adding peaches (should show true) ', addItem( 'peaches', basket ));
 console.log( `Basket is now ${basket.join(', ')}` );
 console.log('Trying to remove apples from basket (should return apples)', removeItem( 'apples', basket ));
-console.log(`Basket now contains ${basket}`);
+console.log(`Basket now contains ${basket.join(', ')}`);
 console.log('Trying to remove apples from basket (should return null)', removeItem( 'apples', basket ));
-console.log(`Basket is now contains ${basket}`);
+console.log(`Basket now contains ${basket.join(', ')}`);
+}
 
-window.onload = function writeIndex() {
-document.getElementById("test").innerHTML = `Basket is now ${basket.join(', ')}`;
+function questBtn () {
+    if ( document.getElementById('list').style.display === 'none' ) {
+    document.getElementById('list').style.display = 'block';
+    document.getElementById('questionBtn').style.backgroundColor = '#52ffb8';
+    document.getElementById('questionBtn').style.color = '#615756';
+    document.getElementById('questionBtn').innerText = 'I am done!'
+    } else {
+        document.getElementById('list').style.display = 'none';
+        document.getElementById('questionBtn').innerText = 'Questions?';
+        document.getElementById('questionBtn').style.backgroundColor = '#615756';
+        document.getElementById('questionBtn').style.color = '#52ffb8';
+    }
 }
